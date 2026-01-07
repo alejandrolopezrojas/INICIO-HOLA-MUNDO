57540853e3982099d9c19227076c86bb441ce78a
alejandrolopezrojas <alejandrolopezrojas2009@gmail.com>
2026-01-07 12:25:07 -0500

    primera operación

---
+++ hola.py
@@ -0,0 +1,25 @@
+#este es mi primer codigo en python, esto es un comentario
+# lo que voy a hacer es un hola mundo, solo es usar print para imprimir un mensaje           
+print("hola ingenieria en inteligencia artificial")
+
+"""
+este es
+un comentario
+en varias
+lineas diferentes
+"""
+#se pueden usar comillas dobles o simples tambien
+print('hola python, este es mi primer proyecto')
+'''
+este tambien
+es un comentario
+en varias lineas
+'''
+
+#el type se usa para que se nos diga que tipo de dato que es algo
+print(type('hola python, este es mi primer proyecto')) #tipo "str"
+print(type(5)) #tipo "int"
+print(type(1.5)) #tipo "float"
+print(type(True)) #tipo "bool"
+print(type(6 + 2j)) #tipo "complex"
+#hola, sigo sin entender bien como funciona el guardado en github
