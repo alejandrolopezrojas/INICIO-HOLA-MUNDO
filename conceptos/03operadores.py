823550ce468ad2b79c1c7ea5ecabe20676b944e6
alejandrolopezrojas <alejandrolopezrojas2009@gmail.com>
2026-01-08 11:25:49 -0500

    Agregar contenido inicial sobre operadores matemáticos

---
+++ conceptos/03operadores.py
@@ -0,0 +1,15 @@
+"""los operadores hacen operaciones
+ matematicas, estos son algunos basicos"""
+
+print(10+3) #SUMA
+print(10-3) #RESTA
+print(10*3) #MULTIPLICACIÓN
+print(10/3) #DIVISIÓN
+
+#ahora algunos mas avanzados
+
+print(10**3) #EXPONENTE
+print(10%3) #TE DICE CUENTO SOBRA DESPUES DE LA DIVISIÓN
+print(10//3) #FLOORDIVICIÓN SIEMPRE APROXIMA A UN ENTERO
+
+
