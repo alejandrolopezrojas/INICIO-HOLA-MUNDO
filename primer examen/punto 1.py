cb4a4d9d1cb35526f39a3759aa58290ed38cd7ad
alejandrolopezrojas <alejandrolopezrojas2009@gmail.com>
2026-01-07 22:55:20 -0500

    avanzar en el examen hasta el punto 4

--- primer examen/punto 1.py
+++ primer examen/punto 1.py
@@ -4,3 +4,36 @@ edad= 16
 nombre= "Alejandro"
 altura= 1.83
 print("hola, mi nombre es", nombre, "mi edad es", edad, "y mi altura es", altura)
+
+
+
+#punto 2
+a = 10
+b = 3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+
+
+#punto 3
+
+nombre = input("ingrese su nombre= ")
+edad = int(input("ingrese su edad= "))
+print("nombre registrado=", nombre)
+print("esta es tu edad el proximo año= ", edad+1)
+
+
+
+#punto 4
+
+n1, n2 = int(input("ingrese n1= ")), int(input("ingrese n2= "))
+print(n1+n2)
+print(n1*n2)
+
+
+
+
+
